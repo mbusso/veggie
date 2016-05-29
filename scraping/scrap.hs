9b77299e5281ae47c4dc147f99f12c3265a90361
mbusso <mmatiassbb@gmail.com>
2016-05-29 18:42:34 +0000

    save bazar to file

--- scraping/scrap.hs
+++ scraping/scrap.hs
@@ -8,4 +8,5 @@ main = do
     let doc = HandsomeSoup.fromUrl "http://www.legrandpere.com.ar/BAZAR-BAZAR"
     productos <- runX $ (doc >>> css "table tbody tr"  >>> css "td:nth-child(1), td:nth-child(2) div:nth-child(1)" /> getText) >>. L.filter ((> 0) . T.length . strip . pack)
     mapM_ putStrLn productos
+    writeFile "scraping/bazar" $ L.unwords productos
 
