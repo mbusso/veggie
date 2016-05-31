c13c470245d7da0f84ec0d948734b623a966e42e
mbusso <mmatiassbb@gmail.com>
2016-05-31 02:10:39 +0000

    create getDocument function

--- scraping/scrap.hs
+++ scraping/scrap.hs
@@ -3,10 +3,21 @@ import Text.XML.HXT.Core
 import Text.HandsomeSoup as HandsomeSoup
 import Data.Text.Lazy as T
 import Data.List as L
+import Text.XML.HXT.Arrow.XmlState.TypeDefs
+import Data.Tree.NTree.TypeDefs
+import Text.XML.HXT.DOM.TypeDefs
 
 main = do
-    let doc = HandsomeSoup.fromUrl "http://www.legrandpere.com.ar/BAZAR-BAZAR"
+    let doc = getDocument "http://www.legrandpere.com.ar/BAZAR-BAZAR"
     productos <- runX $ (doc >>> css "table tbody tr"  >>> css "td:nth-child(1), td:nth-child(2) div:nth-child(1)" /> getText) >>. L.filter ((> 0) . T.length . strip . pack)
     mapM_ putStrLn productos
-    writeFile "scraping/bazar" $ L.unwords productos
+    -- writeFile "scraping/bazar" $ L.unwords productos
 
+
+-- getBazarProducts :: [Char]
+--getBazarProducts = 
+--	HandsomeSoup.fromUrl "http://www.legrandpere.com.ar/BAZAR-BAZAR" >>= \ doc ->
+--    runX((doc >>> css "table tbody tr"  >>> css "td:nth-child(1), td:nth-child(2) div:nth-child(1)" /> getText) >>. L.filter ((> 0) . T.length . strip . pack))
+
+getDocument:: String -> IOSArrow b (NTree XNode)
+getDocument  = HandsomeSoup.fromUrl 
