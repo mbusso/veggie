71e3d8d04b95b23d827b1e71ed8b61487a5a2c45
mbusso <mmatiassbb@gmail.com>
2016-05-31 02:16:40 +0000

    create filter function

--- scraping/scrap.hs
+++ scraping/scrap.hs
@@ -9,7 +9,7 @@ import Text.XML.HXT.DOM.TypeDefs
 
 main = do
     let doc = getDocument "http://www.legrandpere.com.ar/BAZAR-BAZAR"
-    productos <- runX $ (doc >>> css "table tbody tr"  >>> css "td:nth-child(1), td:nth-child(2) div:nth-child(1)" /> getText) >>. L.filter ((> 0) . T.length . strip . pack)
+    productos <- runX $ (doc >>> css "table tbody tr"  >>> css "td:nth-child(1), td:nth-child(2) div:nth-child(1)" /> getText) >>. L.filter (filterEmptyStrings)
     mapM_ putStrLn productos
     -- writeFile "scraping/bazar" $ L.unwords productos
 
@@ -20,4 +20,7 @@ main = do
 --    runX((doc >>> css "table tbody tr"  >>> css "td:nth-child(1), td:nth-child(2) div:nth-child(1)" /> getText) >>. L.filter ((> 0) . T.length . strip . pack))
 
 getDocument:: String -> IOSArrow b (NTree XNode)
-getDocument  = HandsomeSoup.fromUrl 
+getDocument  = HandsomeSoup.fromUrl
+
+filterEmptyStrings:: String -> Bool
+filterEmptyStrings = (> 0) . T.length . strip . pack
