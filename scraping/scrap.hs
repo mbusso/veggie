8987f0530e9e6c9989a69624279f67faf6d68c51
mbusso <mmatiassbb@gmail.com>
2016-06-05 20:29:42 +0000

    create selector function

--- scraping/scrap.hs
+++ scraping/scrap.hs
@@ -9,18 +9,20 @@ import Text.XML.HXT.DOM.TypeDefs
 
 main = do
     let doc = getDocument "http://www.legrandpere.com.ar/BAZAR-BAZAR"
-    productos <- runX $ (doc >>> css "table tbody tr"  >>> css "td:nth-child(1), td:nth-child(2) div:nth-child(1)" /> getText) >>. L.filter (filterEmptyStrings)
+    productos <- runX $ (doc >>> bazarSelector /> getText) >>. L.filter (filterEmptyStrings)
     mapM_ putStrLn productos
     -- writeFile "scraping/bazar" $ L.unwords productos
 
-
--- getBazarProducts :: [Char]
---getBazarProducts = 
---	HandsomeSoup.fromUrl "http://www.legrandpere.com.ar/BAZAR-BAZAR" >>= \ doc ->
---    runX((doc >>> css "table tbody tr"  >>> css "td:nth-child(1), td:nth-child(2) div:nth-child(1)" /> getText) >>. L.filter ((> 0) . T.length . strip . pack))
 
 getDocument:: String -> IOSArrow b (NTree XNode)
 getDocument  = HandsomeSoup.fromUrl
 
 filterEmptyStrings:: String -> Bool
 filterEmptyStrings = (> 0) . T.length . strip . pack
+
+bazarSelector:: ArrowXml a => a (NTree XNode) (NTree XNode)
+bazarSelector = HandsomeSoup.css "table tbody tr"  >>> HandsomeSoup.css "td:nth-child(1), td:nth-child(2) div:nth-child(1)"
+
+
+
+
